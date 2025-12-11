9c6b54b0a8061f292e0209124aacd3ebd5b4636c
GitHub Action <action@github.com>
2025-12-11 15:27:40 +0000

    Bump version to 3.1.0
    Version type: minor
    Release notes: 'Standard minor release'

--- src/Client.cs
+++ src/Client.cs
@@ -10,7 +10,7 @@ namespace GetStream
 {
     public class BaseClient :IClient
     {
-        private const string VersionName = "3.0.0";
+        private const string VersionName = "3.1.0";
         private static readonly string VersionHeader = $"getstream-net-{VersionName}";
         
         private readonly HttpClient _httpClient;
