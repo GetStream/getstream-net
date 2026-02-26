26ceeaaada9ee9849523f9addec909310f6c1868
Yun Wang <yun.wang@getstream.io>
2026-02-26 14:12:14 +0100

    feat: add MuteUnmuteUser moderation integration test

--- tests/ChatModerationIntegrationTests.cs
+++ tests/ChatModerationIntegrationTests.cs
@@ -26,6 +26,50 @@ namespace GetStream.Tests
             DefaultIgnoreCondition = System.Text.Json.Serialization.JsonIgnoreCondition.WhenWritingNull
         };
 
+        [Test, Order(2)]
+        public async Task MuteUnmuteUser()
+        {
+            // Create 2 users: muter and target
+            var userIds = await CreateTestUsers(2);
+            var muterId = userIds[0];
+            var targetId = userIds[1];
+
+            // Mute the target user (without timeout)
+            var muteResp = await _moderationClient.MuteAsync(new MuteRequest
+            {
+                TargetIds = new List<string> { targetId },
+                UserID = muterId
+            });
+            Assert.That(muteResp.Data, Is.Not.Null);
+            Assert.That(muteResp.Data!.Mutes, Is.Not.Null);
+            Assert.That(muteResp.Data!.Mutes, Is.Not.Empty, "Mute response should contain mutes");
+
+            var mute = muteResp.Data!.Mutes[0];
+            Assert.That(mute.User, Is.Not.Null, "Mute should have a User");
+            Assert.That(mute.Target, Is.Not.Null, "Mute should have a Target");
+            Assert.That(mute.Expires, Is.Null, "Mute without timeout should have no Expires");
+
+            // Verify mute appears in QueryUsers for the muting user
+            var qResp = await QueryUsers(new QueryUsersPayload
+            {
+                FilterConditions = new Dictionary<string, object>
+                {
+                    ["id"] = new Dictionary<string, object> { ["$eq"] = muterId }
+                }
+            });
+            Assert.That(qResp.Data, Is.Not.Null);
+            Assert.That(qResp.Data!.Users, Is.Not.Empty);
+            Assert.That(qResp.Data!.Users[0].Mutes, Is.Not.Empty, "User should have Mutes after muting");
+
+            // Unmute the user
+            var unmuteResp = await _moderationClient.UnmuteAsync(new UnmuteRequest
+            {
+                TargetIds = new List<string> { targetId },
+                UserID = muterId
+            });
+            Assert.That(unmuteResp.Data, Is.Not.Null);
+        }
+
         [Test, Order(1)]
         public async Task BanUnbanUser()
         {
