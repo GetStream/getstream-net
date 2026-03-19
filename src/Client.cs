64ede4b39a62694b9e62f1919bc9a7787c04da13
GitHub Action <action@github.com>
2026-03-19 17:22:19 +0000

    chore: bump version to 7.0.4 [skip ci]

--- src/Client.cs
+++ src/Client.cs
@@ -10,7 +10,7 @@ namespace GetStream
 {
     public class BaseClient : IClient
     {
-        private const string VersionName = "7.0.3";
+        private const string VersionName = "7.0.4";
         private static readonly string VersionHeader = $"getstream-net-{VersionName}";
 
         private readonly HttpClient _httpClient;
