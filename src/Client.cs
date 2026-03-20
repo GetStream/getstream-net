0505211313a137be4cdaf33f557a963f625b11c0
GitHub Action <action@github.com>
2026-03-20 16:38:03 +0000

    Bump version to 7.1.0
    Version type: minor
    Release notes: 'Standard minor release'

--- src/Client.cs
+++ src/Client.cs
@@ -10,7 +10,7 @@ namespace GetStream
 {
     public class BaseClient : IClient
     {
-        private const string VersionName = "7.0.4";
+        private const string VersionName = "7.1.0";
         private static readonly string VersionHeader = $"getstream-net-{VersionName}";
 
         private readonly HttpClient _httpClient;
